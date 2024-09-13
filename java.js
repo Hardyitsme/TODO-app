fb73ee9cec9b8f633759de5f6bb7862a15536cb6
Hardyitsme <orupod@mail.ru>
2024-09-13 17:26:46 +0300

    Добавлена кнопка удаления и время

--- java.js
+++ java.js
@@ -1,16 +1,79 @@
-function addItem(){
-    const inputField = document.getElementById('inputField');
-    const inputValue = inputField.value.trim()
+const displayArea = document.getElementById("displayArea");
 
-    if(inputField !== ''){
-        const displayArea = document.getElementById("displayArea");
-        const newArea = document.createElement('div')
-        newArea.className = 'item';
-        newArea.textContent = inputValue;
+function getInputValue() {
+  const inputField = document.getElementById("inputField");
+  const inputValue = inputField.value.trim();
 
-        displayArea.appendChild(newArea);
-
-        inputField.value = '';
-
-    }
+  return inputValue;
 }
+
+function clearInput() {
+  const inputField = document.getElementById("inputField");
+
+  inputField.value = "";
+}
+
+function formatDate(date) {
+  // Получаем день, месяц и год
+  const day = date.getDate().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Месяцы начинаются с 0
+  const year = date.getFullYear();
+
+  // Форматируем строку
+  return `${day}-${month}-${year}`;
+}
+
+
+
+function addItem(itemText) {
+  const newItem = document.createElement("div");
+  newItem.className = "task-item";
+  newItem.textContent = itemText;
+
+  displayArea.appendChild(newItem);
+
+
+  const dateItem = document.createElement('div')
+  dateItem.className = 'date-item'
+ 
+  const currentDate = new Date();
+  dateItem.textContent = formatDate(currentDate)
+ 
+
+  newItem.appendChild(dateItem)
+
+  const deleteButton = document.createElement('button')
+  deleteButton.className = "delete-item";
+  deleteButton.textContent = ('Delete');
+
+  newItem.appendChild(deleteButton)
+
+  deleteButton.addEventListener("click", function () {
+    newItem.remove();
+  });
+}
+
+const addButton = document.querySelector("button");
+
+addButton.addEventListener("click", function () {
+  const inputValue = getInputValue();
+
+  if (inputValue.length) {
+    addItem(inputValue);
+    clearInput();
+  }
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    const inputValue = getInputValue();
+
+    if (inputValue.length) {
+      addItem(inputValue);
+      clearInput();
+    }
+  }
+});
+
+
+
